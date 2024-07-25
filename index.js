843aeefa4a5f160cfc21ee504ccd3ab5bd19f454
sharmavijay07 <156803752+sharmavijay07@users.noreply.github.com>
2024-07-25 20:53:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
+const app = express();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const io = new Server({
+  cors: true,
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(bodyParser.json());
+
+const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
+
+io.on("connection", (socket) => {
+  console.log("new connection");
+
+  socket.on("join-room", (data) => {
+    const { emailId, roomId } = data;
+    console.log("user ", emailId, "joined room ", roomId);
+    emailToSocketMapping.set(emailId, socket.id);
+    socketToEmailMapping.set(socket.id, emailId);
+    socket.join(roomId);
+    socket.emit("joined-room", { roomId });
+    socket.broadcast.to(roomId).emit("user-joined", { emailId });
+  });
+
+  socket.on('call-user', data => {
+    const { emailId, offer } = data;
+    const fromEmail = socketToEmailMapping.get(socket.id)
+    const socketId = emailToSocketMapping.get(emailId)
+    socket.to(socketId).emit('incomming-call', { from: fromEmail, offer })
+
+  });
+
+  socket.on('call-accepted', data => {
+    const { emailId, ans } = data;
+    const socketId = emailToSocketMapping.get(emailId);
+    socket.to(socketId).emit('call-accepted', { ans })
+  })
+});
+
+app.listen(8000, () => {
+  console.log("http server is running on 8000");
+});
+io.listen(8001);
